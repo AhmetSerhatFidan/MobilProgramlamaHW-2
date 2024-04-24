8e2c755ac2c625ee3fc255caa588303ca00f3fb5
200101024@ogrenci.yalova.edu.tr <200101024@ogrenci.yalova.edu.tr>
2024-04-24 17:27:58 +0300

    hw_2

---
+++ app/src/main/java/com/ahmetserhatfidan/hw_2/Item.kt
@@ -0,0 +1,9 @@
+package com.ahmetserhatfidan.hw_2
+
+
+data class Item(
+    val title: String,
+    val description: String,
+    var isChecked: Boolean = false
+)
+
---
+++ app/src/main/java/com/ahmetserhatfidan/hw_2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ahmetserhatfidan.hw_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
